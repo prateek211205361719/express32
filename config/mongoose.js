d7e01bc9beed1b0e2ea7c2012d50215d46f8776d
prateek211205361719 <prateek@ceptes.com>
2017-08-11 13:02:30 +0530

    config change

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,8 +3,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://prateek211205:sanu211205@ds058369.mlab.com:61262/todoapp');
-
+mongoose.connect('mongodb://prateek211205:sanu211205@ds161262.mlab.com:61262/todoapp');
 module.exports = {
     mongoose
 };
