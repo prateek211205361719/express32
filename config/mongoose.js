70b8c8164245aeb71d7bd934eddb7850f5dda36a
prateek211205361719 <prateek@ceptes.com>
2017-08-11 12:49:21 +0530

    seeting mongolab

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,9 @@
 
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+mongoose.connect('mongodb://prateek211205:sanu211205@ds058369.mlab.com:61262/todoapp');
+
 module.exports = {
     mongoose
 };
